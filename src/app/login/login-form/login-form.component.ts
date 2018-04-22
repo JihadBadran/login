7c6581019a23cf4a66a1497dc0428ea40acd9b2c
jihadbadran <jihadm_2009@hotmail.com>
2018-04-22 10:26:21 +0300

    1

---
+++ src/app/login/login-form/login-form.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit, Inject} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+
+  sentAuth = false;
+
+
+  emailAuth: FormGroup;
+  secondStepLogin: FormGroup;
+
+  constructor(@Inject(FormBuilder) fb: FormBuilder) {
+
+    this.emailAuth = fb.group({
+      email: ['', Validators.email],
+      remember: [false]
+    });
+
+    this.secondStepLogin = fb.group({
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+      validationCode: ['', Validators.required]
+    });
+
+  }
+
+  ngOnInit() {
+  }
+
+  /**
+   * method that check if email is already registered or not
+   * return boolean
+   */
+  validateEmail() {
+    if (this.emailAuth.valid) {
+      this.sentAuth = true;
+      console.log('clicked');
+      return true;
+    }
+  }
+
+  backStep() {
+    this.sentAuth = false;
+  }
+
+  loginHandler() {
+
+  }
+}
